298fa52619f03711a9d37c87010e5788f5ad21e2
jaguilar0047@gmail.com <Jaguilar0047@gmail.com>
2022-06-24 16:51:23 -0700

    save

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 var today = moment();
 $("#currentDay").text(today.format("hh:mm" + ", " + "MMM Do, YYYY"));
 
+$(document).ready(function () {
+  $(".saveBtn").on("click", function () {
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, text);
+  });
+
+
 // var time = moment();
 // $("#currentDay").text(time.format("hh"));
 
